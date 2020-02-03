f7c93bcacb8f23d6530875ed0cc9c5ef8baed394
MegY <meghdadyazdi@yahoo.com>
2020-02-03 20:47:59 +0000

    styling forms

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -5,3 +5,15 @@ $(document).ready(function() {
     $('.modal').modal();
     $('.sidenav').sidenav();
   });
+$('#textarea1').val('New Text');
+  M.textareaAutoResize($('#textarea1'));
+
+//   $(document).ready(function(){
+//     $('input.autocomplete').autocomplete({
+//       data: {
+//         "Apple": null,
+//         "Microsoft": null,
+//         "Google": 'https://placehold.it/250x250'
+//       },
+//     });
+//   });
