56409e6f87fdd80b797764d827fd2359ae06955f
MegY <meghdadyazdi@yahoo.com>
2020-02-03 15:20:25 +0000

    add viewport

---
+++ static/js/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+    $('select').formSelect();
+    $(".dropdown-trigger").dropdown();
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+  });
