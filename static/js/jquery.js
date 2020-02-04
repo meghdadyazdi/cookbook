30f966dca252960ae4396f35e3da3b8c8e278818
MegY <meghdadyazdi@yahoo.com>
2020-02-04 23:46:32 +0000

    add footer

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
-    $('input#input_text, textarea#textarea2').characterCounter();
     $('select').formSelect();
     $(".dropdown-trigger").dropdown();
     $('.modal').modal();
     $('.sidenav').sidenav();
-  });
-$('#textarea1').val('New Text');
-  M.textareaAutoResize($('#textarea1'));
+  
+	
+	// INITIATE THE FOOTER
+  siteFooter();
+	// COULD BE SIMPLIFIED FOR THIS PEN BUT I WANT TO MAKE IT AS EASY TO PUT INTO YOUR SITE AS POSSIBLE
+	$(window).resize(function() {
+		siteFooter();
+	});
+	
+	function siteFooter() {
+		var siteContent = $('#site-content');
+		var siteContentHeight = siteContent.height();
+		var siteContentWidth = siteContent.width();
 
-//   $(document).ready(function(){
-//     $('input.autocomplete').autocomplete({
-//       data: {
-//         "Apple": null,
-//         "Microsoft": null,
-//         "Google": 'https://placehold.it/250x250'
-//       },
-//     });
-//   });
+		var siteFooter = $('#site-footer');
+		var siteFooterHeight = siteFooter.height();
+		var siteFooterWidth = siteFooter.width();
+
+		console.log('Content Height = ' + siteContentHeight + 'px');
+		console.log('Content Width = ' + siteContentWidth + 'px');
+		console.log('Footer Height = ' + siteFooterHeight + 'px');
+		console.log('Footer Width = ' + siteFooterWidth + 'px');
+
+		siteContent.css({
+			"margin-bottom" : siteFooterHeight + 50
+		});
+	};
+});
