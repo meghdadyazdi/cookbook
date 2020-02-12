6da8b0ab23a0fe406a89adcfc144ba04577b513f
MegY <meghdadyazdi@yahoo.com>
2020-02-12 23:12:57 +0000

    validating CSS

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -3,84 +3,40 @@ $(document).ready(function () {
     $(".dropdown-trigger").dropdown();
     $('.modal').modal();
     $('.sidenav').sidenav();
-
-
-    // INITIATE THE FOOTER
-    siteFooter();
-    // COULD BE SIMPLIFIED FOR THIS PEN BUT I WANT TO MAKE IT AS EASY TO PUT INTO YOUR SITE AS POSSIBLE
-    $(window).resize(function () {
-        siteFooter();
-    });
-
-    function siteFooter() {
-        var siteContent = $('#site-content');
-        var siteContentHeight = siteContent.height();
-        var siteContentWidth = siteContent.width();
-
-        var siteFooter = $('#site-footer');
-        var siteFooterHeight = siteFooter.height();
-        var siteFooterWidth = siteFooter.width();
-
-        // console.log('Content Height = ' + siteContentHeight + 'px');
-        // console.log('Content Width = ' + siteContentWidth + 'px');
-        // console.log('Footer Height = ' + siteFooterHeight + 'px');
-        // console.log('Footer Width = ' + siteFooterWidth + 'px');
-
-        siteContent.css({
-            "margin-bottom": siteFooterHeight + 50
-        });
-    };
-
-
     $("#signinDrop").click(function () {
         if ($('#signinBox').is(':visible')) {
             $("#signinBox").css("display", "none");
         } else {
             $("#signinBox").css("display", "block");
         };
-        // $("#signinBox").css( "transform", "translateY(0)");
-        // $("#signinBox").css( "z-index", "9999");
     });
 
-
-
-
     var fullDate = new Date()
-    // console.log(fullDate);
-    //Thu May 19 2011 17:25:38 GMT+1000 {}
 
     //convert month to 2 digits
     var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ? (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
-
-    // console.log(fullDate.getMonth());
-
     var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-    // console.log(fullDate[1]);
-
-    // $("#recipe_date").val(currentDate)
     $("#recipe_date").val(fullDate)
 
 
 
 
+    // ____________________________rating stars_______________________________
 
-
-
+    // __________mouse enter___________    
     $(".star-1").mouseenter(function () {
-        $(this).addClass("rate1"); 
+        $(this).addClass("rate1");
     });
 
     $(".star-2").mouseenter(function () {
-        $(this).addClass("rate2"); 
+        $(this).addClass("rate2");
         $(".star-1").addClass("rate2");
-        // return false;
     });
 
     $(".star-3").mouseenter(function () {
         $(this).addClass("rate3");
         $('.star-2').addClass("rate3");
         $('.star-1').addClass("rate3");
-        // return false;
     });
 
     $(".star-4").mouseenter(function () {
@@ -88,7 +44,6 @@ $(document).ready(function () {
         $('.star-3').addClass("rate4");
         $('.star-2').addClass("rate4");
         $('.star-1').addClass("rate4");
-        // return false;
     });
 
     $(".star-5").mouseenter(function () {
@@ -97,26 +52,23 @@ $(document).ready(function () {
         $('.star-3').addClass("rate5");
         $('.star-2').addClass("rate5");
         $('.star-1').addClass("rate5");
-        // return false;
     });
 
-// __________mouse leave___________
+    // __________mouse leave___________
 
     $(".star-1").mouseleave(function () {
-        $(this).removeClass("rate1"); 
+        $(this).removeClass("rate1");
     });
 
     $(".star-2").mouseleave(function () {
-        $(this).removeClass("rate2"); 
+        $(this).removeClass("rate2");
         $(".star-1").removeClass("rate2");
-        // return false;
     });
 
     $(".star-3").mouseleave(function () {
         $(this).removeClass("rate3");
         $('.star-2').removeClass("rate3");
         $('.star-1').removeClass("rate3");
-        // return false;
     });
 
     $(".star-4").mouseleave(function () {
@@ -124,7 +76,6 @@ $(document).ready(function () {
         $('.star-3').removeClass("rate4");
         $('.star-2').removeClass("rate4");
         $('.star-1').removeClass("rate4");
-        // return false;
     });
 
     $(".star-5").mouseleave(function () {
@@ -133,7 +84,6 @@ $(document).ready(function () {
         $('.star-3').removeClass("rate5");
         $('.star-2').removeClass("rate5");
         $('.star-1').removeClass("rate5");
-        // return false;
     });
 
 
@@ -145,7 +95,7 @@ $(document).ready(function () {
         $(".star-3").removeClass("rate33 rate44 rate55");
         $(".star-4").removeClass("rate44 rate55");
         $(".star-5").removeClass("rate55");
-         $(this).addClass("rate11");
+        $(this).addClass("rate11");
         $("#recipe_rate11").val("1")
         $("#recipe_rate22").val("")
         $("#recipe_rate33").val("")
@@ -154,7 +104,7 @@ $(document).ready(function () {
     });
 
     $(".star-2").click(function () {
-        $(".star-1").removeClass("rate33 rate44 rate55");        
+        $(".star-1").removeClass("rate33 rate44 rate55");
         $(this).removeClass("rate22 rate33 rate44 rate55");
         $(".star-3").removeClass("rate33 rate44 rate55");
         $(".star-4").removeClass("rate44 rate55");
@@ -171,10 +121,9 @@ $(document).ready(function () {
     $(".star-3").click(function () {
         $(".star-1").removeClass("rate44 rate55");
         $(".star-2").removeClass("rate44 rate55");
-        // $(".satr-4").removeClass("rate44");
         $(this).removeClass("rate33 rate44 rate55");
         $(".star-4").removeClass("rate44 rate55");
-         $(".star-5").removeClass("rate55");
+        $(".star-5").removeClass("rate55");
         $(this).addClass("rate33");
         $(".star-1").addClass("rate33");
         $(".star-2").addClass("rate33");
@@ -185,10 +134,10 @@ $(document).ready(function () {
         $("#recipe_rate55").val("")
     });
 
-    $(".star-4").click(function () {        
+    $(".star-4").click(function () {
         $(".star-1").removeClass("rate55");
         $(".star-2").removeClass("rate55");
-        $(".star-3").removeClass("rate55");        
+        $(".star-3").removeClass("rate55");
         $(this).removeClass("rate44 rate55");
         $(".star-5").removeClass("rate55");
         $(this).addClass("rate44");
@@ -215,36 +164,7 @@ $(document).ready(function () {
         $("#recipe_rate55").val("5")
     });
 
-    
-
-
-
-        // var str = $("#recipe_rate11").val(1);
-        // console.log(str);
-        // var rate1 = parseInt("str");
-        // console.log(rate1);
-        // rate1 += 1;
-        // console.log(rate1);
-        // rate1.toString();
-        // console.log(rate1);
-        // $("#recipe_rate1").val(rate1)
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Or use this to Open link in same window (similar to target=_blank)
+    // Open link in same window (similar to target=_blank)
     $(".boxxx").click(function () {
         window.location = $(this).find("a:first").attr("href");
         return false;
